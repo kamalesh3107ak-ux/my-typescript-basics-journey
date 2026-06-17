7ed920d38bc4d337dd355e54f8a0622a9f066392
Kamalesh <kamalesh3107ak@gmail.com>
2026-06-17 09:45:37 +0530

    Initial TypeScript practice files

---
+++ basics.ts
@@ -0,0 +1,4 @@
+//console.log("This is a basic TypeScript file.");
+let message: string = "Hello, TypeScript!";
+console.log(message);
+
---
+++ conditional.statements.ts
@@ -0,0 +1,79 @@
+// conditional.statement 
+// types of conditional statements
+// 1. if statement
+// 2. if...else statement
+// 3. switch statement
+// 4. nested if statement
+
+
+//1. if else statement
+/*let studentname : string = "kamalesh"
+let persentage : number = 20;
+if (persentage  >= 18) 
+{
+    console.log(studentname,persentage, "eligible for voting");
+} 
+else
+{
+    console.log(studentname,persentage, "not eligible for voting");
+}
+
+//2.else if statement
+let marks : number = 85;
+if (marks >= 90)
+{
+    console.log("Grade A");
+}
+else if (marks >= 75)
+{
+    console.log("Grade B");
+}
+else if (marks >= 50)
+{
+    console.log("Grade C");
+}
+
+//3. switch statement
+let day : number = 3;
+switch (day)
+{
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+}
+*/
+
+
+//4. nested if statement
+/*let age : number = 70;
+if (age >= 18)
+{
+    if (age <= 60)
+    {
+        console.log("Eligible for work");
+    }
+    else
+    {
+        console.log("Not eligible for work");
+    }
+}*/
